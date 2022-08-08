0535e80e92fbc29e402eea5cbde593a21b5a160a
André Martins <aanm90@gmail.com>
2022-08-08 23:27:45 +0200

    Revert "client-go: remove no longer used finalURLTemplate"
    The functionality provided by the finalURLTemplate is still used by
    certain external projects to track the request latency for requests
    performed to kube-apiserver.
    
    Using a template of the URL, instead of the URL itself, prevents the
    explosion of label cardinality in exposed metrics since it aggregates
    the URLs in a way that common URLs requests are reported as being the
    same.
    
    This reverts commit bebf5a608f68523fc430a44f6db26b16022dc862.
    
    Signed-off-by: André Martins <aanm90@gmail.com>
    
    Kubernetes-commit: 11e16c63c87f07e9e68977c74b937989983754c1

--- rest/request.go
+++ rest/request.go
@@ -527,17 +527,14 @@ func (r Request) finalURLTemplate() url.URL {
 		newParams[k] = v
 	}
 	r.params = newParams
-	u := r.URL()
-	if u == nil {
-		return url.URL{}
-	}
+	url := r.URL()
 
-	segments := strings.Split(u.Path, "/")
+	segments := strings.Split(url.Path, "/")
 	groupIndex := 0
 	index := 0
 	trimmedBasePath := ""
-	if r.c.base != nil && strings.Contains(u.Path, r.c.base.Path) {
-		p := strings.TrimPrefix(u.Path, r.c.base.Path)
+	if url != nil && r.c.base != nil && strings.Contains(url.Path, r.c.base.Path) {
+		p := strings.TrimPrefix(url.Path, r.c.base.Path)
 		if !strings.HasPrefix(p, "/") {
 			p = "/" + p
 		}
@@ -548,7 +545,7 @@ func (r Request) finalURLTemplate() url.URL {
 		groupIndex = 1
 	}
 	if len(segments) <= 2 {
-		return *u
+		return *url
 	}
 
 	const CoreGroupPrefix = "api"
@@ -566,11 +563,11 @@ func (r Request) finalURLTemplate() url.URL {
 		// outlet here in case more API groups are added in future if ever possible:
 		// https://kubernetes.io/docs/concepts/overview/kubernetes-api/#api-groups
 		// if a wrong API groups name is encountered, return the {prefix} for url.Path
-		u.Path = "/{prefix}"
-		u.RawQuery = ""
-		return *u
+		url.Path = "/{prefix}"
+		url.RawQuery = ""
+		return *url
 	}
-	// switch segLength := len(segments) - index; segLength {
+	//switch segLength := len(segments) - index; segLength {
 	switch {
 	// case len(segments) - index == 1:
 	// resource (with no name) do nothing
@@ -593,8 +590,8 @@ func (r Request) finalURLTemplate() url.URL {
 			segments[index+3] = "{name}"
 		}
 	}
-	u.Path = path.Join(trimmedBasePath, path.Join(segments...))
-	return *u
+	url.Path = path.Join(trimmedBasePath, path.Join(segments...))
+	return *url
 }
 
 func (r *Request) tryThrottleWithInfo(ctx context.Context, retryInfo string) error {
